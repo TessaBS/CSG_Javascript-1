44875c4883cfb6e902f9f89b2c59c3ad467a4544
TessaBS <153128@csgaugustinus.nl>
2022-09-21 10:01:17 +0000

    x

--- JS/H2/H2O13.js
+++ JS/H2/H2O13.js
@@ -1,10 +1,10 @@
-var aantalRijenRaster = 6;
-var aantalKolommenRaster = 9;
+var aantalRijenRaster = 12;
+var aantalKolommenRaster = 18;
 var celGrootte;
 
 var animatie = [];
 var aantalBeeldjes = 6;
-var nummer = 3;
+var nummer = 0;
 
 var frame;
 var xJos = 400;
@@ -33,21 +33,25 @@ function draw() {
 
   if (keyIsDown(LEFT_ARROW)) {
     xJos -= celGrootte;
+    nummer = 2;
   }
   if (keyIsDown(RIGHT_ARROW)) {
     xJos += celGrootte;
+    nummer = 1;
   }
   if (keyIsDown(UP_ARROW)) {
     yJos-=celGrootte;
+    nummer = 4;
   }
   if (keyIsDown(DOWN_ARROW)) {
     yJos += celGrootte;
+    nummer = 5;
   }
   
   xJos = constrain(xJos,0,width - celGrootte);
   yJos = constrain(yJos,0,height - celGrootte);
   
-  image(animatie[nummer],xJos,yJos);
+  image(animatie[nummer],xJos,yJos,celGrootte,celGrootte);
 }
 
 function tekenRaster() {
