f3f73b12709dd163f619bfaaaf7e4ab401a7d9b9
TessaBS <153128@csgaugustinus.nl>
2022-11-23 10:31:29 +0000

    x

--- mijnGame/JS/mijnGame.js
+++ mijnGame/JS/mijnGame.js
@@ -28,13 +28,13 @@ class Schermen {
     }
     
     maakBegin() {
-      push();
-        background(252, 204, 243);
+      push(),
+        background(252, 204, 243),
         text('Klik op Enter om het spel te starten. \n'+'Je moet proberen om naar de overkant te komen, door middel van het gebruik van de pijltjestoetsen. ' +
         'Hierbij kom je obstakels tegen, als je een obstakel raakt ben je af en moet je opnieuw beginnen. ' +
         'Je kunt ook een kleine of grote beloning krijgen door deze aan te raken. ' + 
-        'Als je de overkant en voldoende muntjes hebt gehaald, ga je door naar het volgende level.',(windowWidth-1000)/2,100,1000);
-      pop();
+        'Als je de overkant en voldoende muntjes hebt gehaald, ga je door naar het volgende level.',(windowWidth-1000)/2,100,1000),
+      pop(),
     }
   }
 
@@ -111,57 +111,6 @@ function draw() {
 //   image(strand,strandX + strand.width,0);
 //   strandX--;
 //   if (strandX == -strand.width) {
-//     strandX = 0;
-}
-
-
-  function draw() {
-    schermen.maakBegin();
-//     image(strand,strandX,0);
-//     image(strand,strandX + strand.width,0);
-//     strandX--;
-//     if (strandX == -strand.width) {
-//     strandX = 0;
-}
-
-function maakBegin() {
-    push();
-    textAlign(CENTER,CENTER);
-    background(252, 204, 243);
-    text('Klik op Enter om het spel te starten. \n'+'Je moet proberen om naar de overkant te komen, door middel van het gebruik van de pijltjestoetsen. ' +
-    'Hierbij kom je obstakels tegen, als je een obstakel raakt ben je af en moet je opnieuw beginnen. ' +
-    'Je kunt ook een kleine of grote beloning krijgen door deze aan te raken. ' + 
-    'Als je de overkant en voldoende muntjes hebt gehaald, ga je door naar het volgende level.',(windowWidth-1000)/2,100,1000);
-    textSize(20);
-    pop();
-}
-
-// function keyPressed() {
-//   if (keyCode == ENTER) {
-//     function keyTyped() {
-//         if (!spel.actief && !spel.levelGehaald) {
-//           maakBegin();
-//           spel.actief = true;
-//         }
-//         if ((spel.levelGehaald && !spel.afgelopen) && keyCode == ENTER) {
-//           maakBegin();
-//         }
-//         if ((spel.afgelopen)) {
-//           maakBegin();
-//         }  
-//       }
-//     startSpel();
-//     }
-//   }
-
-
-
-function draw() {
-  schermen.maakBegin();
-//     image(strand,strandX,0);
-//     image(strand,strandX + strand.width,0);
-//     strandX--;
-//     if (strandX == -strand.width) {
 //     strandX = 0;
 }
 
