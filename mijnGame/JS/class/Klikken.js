71e68f1ffab2c7720fb0487481f5bf152dd2e8f3
TessaBS <153128@csgaugustinus.nl>
2024-03-08 16:49:01 +0000

    x

--- mijnGame/JS/class/Klikken.js
+++ mijnGame/JS/class/Klikken.js
@@ -3,6 +3,7 @@ class Klikken {
         this.lijst = new Array();
         this.N = 0;
         this.vakNr = 0;
+        this.codeY = 0;
         this.vakX = 0;
         this.vakY = 0;
         this.geel = 1;
@@ -31,7 +32,6 @@ class Klikken {
     // }
 
     plaatsen(){
-
       if(this.klaar == true){
         if(this.vakY < 8){
           if(this.vakX < 8){
@@ -133,23 +133,42 @@ class Klikken {
     codeSchrijven(){
       text('test',50,50,50,50);
 
+      fill(252, 153, 110);
+      rect(0,0,windowWidth,windowHeight);
+      fill(0, 0, 0);
+      textSize(20);
+      text('input.onButtonPressed(Button.A, function () { \n strip.clear()',10,10,windowWidth - 20);
+      text('strip.show() \n }) \n \n input.onButtonPressed(Button.B, function () { \n strip.clear() \n strip.show() \n }) \n let strip: neopixel.Strip = null \n strip = neopixel.create(DigitalPin.P0, 64, NeoPixelMode.RGB) \n }',10,this.codeY + 100,windowWidth - 20);
+      
+      for(this.vakNr = 0;this.vakNr < 64; this.vakNr +=1){
+        this.codeY += 20;
+        text('strip.setPixelColor(' + this.vakNr + ', neopixel.rgb(255, 255, 255))',10,this.codeY,windowWidth - 20);
+      }
 
 
-    //   input.onButtonPressed(Button.A, function () {
-    //     strip.clear()
-    //     strip.setPixelColor(0, neopixel.rgb(255, 255, 255))
-    //     strip.setPixelColor(1, neopixel.rgb(255, 255, 255))
-    //     strip.setPixelColor(2, neopixel.rgb(255, 255, 255))
-    //     strip.show()
-    // })
-    // input.onButtonPressed(Button.B, function () {
-    //     strip.clear()
-    //     strip.show()
-    // })
-    // let strip: neopixel.Strip = null
-    // strip = neopixel.create(DigitalPin.P0, 64, NeoPixelMode.RGB)
-    // }
 
-    // ALLES leeg maken
-    // Knop dat je de code kunct kopiëren? ipv hele code laten zien.
+
+          //   input.onButtonPressed(Button.A, function () {
+          //     strip.clear()
+          //     strip.setPixelColor(0, neopixel.rgb(255, 255, 255))
+          //     strip.setPixelColor(1, neopixel.rgb(255, 255, 255))
+          //     strip.setPixelColor(2, neopixel.rgb(255, 255, 255))
+          //     strip.show()
+          // })
+          // input.onButtonPressed(Button.B, function () {
+          //     strip.clear()
+          //     strip.show()
+          // })
+          // let strip: neopixel.Strip = null
+          // strip = neopixel.create(DigitalPin.P0, 64, NeoPixelMode.RGB)
+          // }
+
+          // ALLES leeg maken
+          // Knop dat je de code kunct kopiëren? ipv hele code laten zien.
+
+
+
+    }
+
+
   }
